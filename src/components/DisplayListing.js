1b94404bba2e9f70047244b5aa4a4a74773415e2
Alex Mao <alexanderpnmao@gmail.com>
2023-08-18 21:05:09 -0500

    added home page and framework for listing cards

--- src/components/DisplayListing.js
+++ src/components/DisplayListing.js
@@ -1,38 +1,34 @@
+import { Box, Heading, Image, SimpleGrid, Text } from "@chakra-ui/react";
 import axios from "axios"
 import { useState, useEffect } from "react";
 
 export default function DisplayListing() {
 
-const [listings, setListings] = useState([]);
+    const [listings, setListings] = useState([]);
 
-useEffect(() => {
-    getListings();
-}, []);
+    useEffect(() => {
+        getListings();
+    }, []);
 
-const getListings = () => {
-    axios.get('http://localhost:31337/api/listings/').then((response)=>{
-        console.log(response.data);    
-        setListings(response.data);
-    });
-}
-console.log(typeof listings)
+    const getListings = () => {
+        axios.get('http://localhost:31337/api/listings/').then((response)=>{
+            console.log(response.data);    
+            setListings(response.data);
+        });
+    }
 
     return(
-        <div>
-            <h1>LISTINGS</h1>
-            <table>
-                <tbody>
-                    {listings.map((listing, key) =>
-                        <tr key={key}>
-                            <td>{listing.id}</td>
-                            <td>{listing.name}</td>
-                            <td>{listing.type}</td>
-                            <td>{listing.price}</td>
-                        </tr>
-                    )}
-                    
-                </tbody>
-            </table>
-        </div>
+        <SimpleGrid columns={3}>
+                {listings.map((listing, key) =>
+                            <Box key={key} bg='black' color='white' w={512} alignItems='center'>
+                                <Image src='https://bit.ly/2Z4KKcF'/>
+                                <Heading>{listing.name}</Heading>
+                                <Text>{listing.type}</Text>
+                                <Text>{listing.rarity}</Text>
+                                <Text>${listing.price}</Text>
+                                <Text>{listing.stock} In Stock</Text>
+                            </Box>
+                        )}
+            </SimpleGrid>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,23 @@
+import { Heading, Image, Stack, Text, VStack, Flex, Button } from '@chakra-ui/react';
+import { Link } from 'react-router-dom'
+import tcgLogo from '../images/pkmn-tcg.png';
+
 export default function Home(){
     return(
-        <div>home</div>
+        <Flex alignContent='center' justifyContent='center' pt='20vh'>
+        
+        <Stack direction='row'>
+            <VStack alignItems='left' justifyContent="center">
+                <Heading size='xl'>Pokémart</Heading>
+                <Text fontSize='xl' maxW='50vh'>
+                   Pokémart is Winnipeg&apos;s premier one stop shop for all things Pokémon TCG related!
+                </Text>
+                <Button>
+                    <Link to='/shop'>Shop</Link>
+                </Button>
+            </VStack>
+           <Image src={tcgLogo} alt='tcg logo' w={512} h={256}/>
+        </Stack>
+        </Flex>
     )
 }
