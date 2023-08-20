f459cd870b591d43a8d64fafd08b234cf9ce21c7
Alex Mao <alexanderpnmao@gmail.com>
2023-08-20 18:50:19 -0500

    created modal for create listing

--- src/components/CreateListing.js
+++ src/components/CreateListing.js
@@ -1,10 +1,9 @@
-import { FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
+import { FormControl, FormLabel, Input, Button, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter } from "@chakra-ui/react";
 import axios from "axios";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 export default function CreateListing() {
-    const navigate = useNavigate();
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     const [inputs, setInputs] = useState({})
 
@@ -15,33 +14,46 @@ export default function CreateListing() {
     }
 
     const handleSubmit = (e) => {
+        onClose();
         e.preventDefault();
         axios.post('http://localhost:31337/api/listings/save', inputs).then((response) => {
             console.log(response.data);
-            navigate('/');
+            window.location.reload(true);
         });
-        console.log(inputs);
     }
 
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-            <FormControl>
-                <FormLabel>Name</FormLabel>
-                <Input type="text" name="name" onChange={handleChange}/>
-                <FormLabel>Type</FormLabel>
-                <Input type="text" name="type" onChange={handleChange}/>
-                <FormLabel>Rarity</FormLabel>
-                <Input type="text" name="rarity" onChange={handleChange}/>
-                <FormLabel>Price</FormLabel>
-                <Input type="number" name="price" onChange={handleChange}/>
-                <FormLabel>Stock</FormLabel>
-                <Input type="number" name="stock" onChange={handleChange}/>
-                <FormLabel>Image</FormLabel>
-                <Input type="file" name="image" onChange={handleChange}/>
-                <Button type="submit">Create Listing</Button>
-            </FormControl>
-            </form>
+            <Button onClick={ onOpen }>Create Listing</Button>
+            <Modal isOpen={ isOpen } onClose={ onClose }>
+                <ModalOverlay/>
+                <ModalContent>
+                    <ModalHeader>Create Listing</ModalHeader>
+                    <ModalCloseButton/>
+                    <ModalBody>
+                        <form>
+                            <FormControl>
+                                <FormLabel>Name</FormLabel>
+                                    <Input type="text" name="name" onChange={handleChange}/>
+                                <FormLabel>Type</FormLabel>
+                                    <Input type="text" name="type" onChange={handleChange}/>
+                                <FormLabel>Rarity</FormLabel>
+                                    <Input type="text" name="rarity" onChange={handleChange}/>
+                                <FormLabel>Price</FormLabel>
+                                    <Input type="number" name="price" onChange={handleChange}/>
+                                <FormLabel>Stock</FormLabel>
+                                    <Input type="number" name="stock" onChange={handleChange}/>
+                                <FormLabel>Image</FormLabel>
+                                    <Input type="file" name="image" onChange={handleChange}/>
+                            </FormControl>
+                        </form>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button onClick={ handleSubmit }>Create Listing</Button>
+                        <Button onClick={ onClose }>Close</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </div>
     )
 }
